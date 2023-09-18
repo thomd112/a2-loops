f2155873dd1e99134ed44a71c8795b2e1f9908e6
thomd112 <114462199+thomd112@users.noreply.github.com>
2023-09-18 08:23:02 -0400

    fizzbuzz.js

--- 1-fizzbuzz/fizzbuzz.js
+++ 1-fizzbuzz/fizzbuzz.js
@@ -1,9 +1,34 @@
 // Your code here
 const fizzBuzz = (n) => {
-  let arr = [];
   
+
+
+  for (let i= 1; i <=n; i ++;){
+
+    if (i%3===0&& i % 5 === 0){
+      else if.push('fizzBuzz');
+    }else if(i % 3 ==== 0 ){
+      result.push('Fizz');
+    } else if(i % 5 ==== 0 ){
+      result.push('Buzz');
+    } else {
+      result.push(i);
+    }
+
+
+      }
+    
+
+
+
+
+
+
+
   return arr;
 }
 // End your code
 
 export { fizzBuzz };
+const fizzBuzzarray= fizzBuzz (5);
+console.log (fizzBuzzarray)
