3bbb8b8495d522c055abe8939241a61f0ef43c5b
thomd112 <114462199+thomd112@users.noreply.github.com>
2023-09-18 08:28:00 -0400

    Update albers.js

--- 2-albers/albers.js
+++ 2-albers/albers.js
@@ -3,7 +3,18 @@ const squares = (initialWidth, n) => {
   let width = initialWidth;
   let color = '#0000ff';
 
+  if (n === 0) {
+    return { width: initialWidth,color:'#0000ff' };
+  }
   
+  for (let i = 1; i <= n; i++) {
+
+    width= width*0.75;
+    
+
+
+
+
 
   return {
     'width': width,
@@ -13,3 +24,4 @@ const squares = (initialWidth, n) => {
 // End your code
 
 export { squares };
+console.log(squares(500, 2));
