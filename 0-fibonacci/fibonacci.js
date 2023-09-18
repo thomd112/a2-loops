bbd3575df73616f7bba9dfa857a483fd8dd7cd20
thomd112 <114462199+thomd112@users.noreply.github.com>
2023-09-18 02:52:22 -0400

    Update fibonacci.js

--- 0-fibonacci/fibonacci.js
+++ 0-fibonacci/fibonacci.js
@@ -1,12 +1,20 @@
 // Your code here
 const fibonacci = (n) => {
   let fn = 0;
-  n=0;
-   n=1;
+  let n=0;
+  let n=1;
+
+  for (let n = 0; i < n; i ++){
+    const Newnumber = fibonacci - 1 + fibonacci - 2;
+  }
+
+  fibonacci.push (Newnumber);
+  Fn = F(n-1) + F(n-2)
   
   return fn;
 }
 // End your code
 
 export { fibonacci };
-const n= 7;
+const n = 7;
+console.log = fibonacci;
